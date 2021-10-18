a762a87d2569a5c5679666fb3b3b80d7c1752114
rasyidnugroho <91443673+rasyidnugroho@users.noreply.github.com>
2021-10-18 08:11:22 +0700

    Add files via upload

---
+++ MetodeGarisLurus.py
@@ -0,0 +1,8 @@
+def MetodeGarisLurus():
+    print("MetodeGarisLurus")
+    Harga = int(input("Masukan harga : Rp"))
+    Residu = int(input("Masukan nilai residu: Rp"))
+    UmurEkonomis = int(input("Masukan umur ekonomis(tahun):"))
+    Hasil = (Harga-Residu)/UmurEkonomis
+    print(Hasil)
+MetodeGarisLurus()    
